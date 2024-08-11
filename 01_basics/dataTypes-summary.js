9ec6b62a86b0be1cbba476968bcb4fa3ac1bf2fb
16rahila <16rahila0@gmail.com>
2024-08-11 19:27:08 +0000

    change in location of file

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 types : String, Number , Boolean(yes or no , true or false), null, undefined, Symbol,
+//  BigInt(scientific values)
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; // in this way value is undefined
+// typeScript: const score:number = 100
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+
+
+const bigNumber = 344355675757866765n
+
+
+//Refrence (Non primitive) values whose value is directly allocated in memory
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myOIbj = {
+    name: "Rahila", //key value pairs, you can store object in variable 
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+    console.log(typeof anotherId);
+
+
+
+
+
