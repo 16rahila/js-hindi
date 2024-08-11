b16cec32fbefd0418d98d778be017887f714aad9
16rahila <16rahila0@gmail.com>
2024-08-11 18:28:57 +0000

    conversion and Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,15 +4,15 @@ let score = true
 // console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
 
 let isLoggedIn = ""
-let booleanIsLoggedIn = Boolean(isLoogeIn)
+//let booleanIsLoggedIn = Boolean(isLoggeddIn)
 // console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
@@ -22,6 +22,55 @@ let booleanIsLoggedIn = Boolean(isLoogeIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//---------------------Opertions-----------------//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //two times for power
+// console.log(2/3);
+// console.log(2%3); //% is for remainder
+
+let str1 = "Hello"
+let str2 = " Rahila"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+//console.log(3 + 4 * 5 % 3); do not write such confusing code
+
+// console.log((3 + 4) * 5  % 3);
+
+//console.log(true+);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+//gameCounter++; postfix operator
+++gameCounter; //prefix operator
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
